6680d6c077d46783f2cd777a5feb0fed151cb14c
srp2930 <s.rimingtonpounder@gmail.com>
2016-06-17 14:28:25 +0100

    email format must be valid

--- app/lib/user.rb
+++ app/lib/user.rb
@@ -7,10 +7,10 @@ attr_reader :password
 attr_accessor :password_confirmation
 
 validates_confirmation_of :password
-validates_format_of :email, as: :email_addressgit
+validates_format_of :email, as: :email_address
 
   property :id, Serial
-  property :email, String, required: true
+  property :email, String, required: true, unique: true
   property :password_digest, String, length: 60
 
   def password=(password)
--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -9,15 +9,24 @@ feature 'user sign up' do
   scenario "requires a matching password confirmation" do
     expect { sign_up(password_confirmation: 'wrong') }.not_to change(User, :count)
     expect(current_path).to eq('/users')
-    expect(page).to have_content('Password and password confirmation do not match')
+    expect(page).to have_content('Password does not match the confirmation')
   end
 
   scenario 'I can\'t sign up without an email address' do
     expect{sign_up(email: nil)}.not_to change(User, :count)
+    expect(current_path).to eq('/users')
+    expect(page).to have_content('Email must not be blank')
   end
 
   scenario 'I can\'t sign up with an invalid email address' do
     expect { sign_up(email: 'invalid@email')}.to change(User, :count).by(0)
+    expect(current_path).to eq('/users')
+    expect(page).to have_content('Email has an invalid format')
   end
 
+  scenario 'I cannot sign up with an existing email' do
+    sign_up
+    expect{ sign_up }.to_not change(User, :count)
+    expect(page).to have_content('Email is already taken')
+  end
 end
